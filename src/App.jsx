d0fb0ab867cdec6418640688553f17c98ce1b4a3
gloriaLVela <gloria.laura.vela@gmail.com>
2025-08-30 08:49:04 -0500

    Syncronize object

--- src/App.jsx
+++ src/App.jsx
@@ -14,13 +14,24 @@ function Header({name, year}){
     const items = [
       "Macaroni and Cheese",
       "Chicken Alfredo",
-      "Caesar Salad"
+      "Caesar Salad",
+      "Spaghetti Bolognese",
+      "Tiramasu"   
+    ];
 
-    ]
+    const dishObjects = items.map((dish, i) =>({
+      id: i,
+      title:dish
+    }));
+    //console.log(dishes);
   function Main({dishes}){
     return (
       <ul>
-        {dishes.map((dish, index) => <li style={{listStyleType:"none"}} key={index}>{dish}</li>)}
+        <ul>
+        {dishes.map((dish) => (
+          <li style={{listStyleType:"none", textAlign:"left"}} key={dish.id}>{dish.title}</li>
+          ))}
+      </ul>
 
       </ul>
     )
@@ -29,7 +40,7 @@ function App() {
   
   return ( <div>
     <Header name="Tere" year={new Date().getFullYear()}/>
-    <Main dishes={items}/>
+    <Main dishes={dishObjects}/>
     </div>);
 }
 
